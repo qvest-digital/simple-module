828c91f4951cd0e0dd1eb54561a646962707ce3e
mirabilos <t.glaser@tarent.de>
2018-09-24 17:10:32 +0200

    hack the regexp until the output matches good enough

--- Build.sh
+++ Build.sh
@@ -4,3 +4,15 @@ set -ex
 
 cd "$(dirname "$0")"
 coffee -c -b --no-header src/simple-module.coffee
+sed --posix \
+    -e "s!{{#if amdModuleId}}'{{amdModuleId}}', {{/if}}!simple-module!g" \
+    -e 's!{{{amdDependencies.wrapped}}}!"jquery"!g' \
+    -e 's!{{{amdDependencies.params}}}!a0!g' \
+    -e "s!{{#if globalAlias}}root\['{{{globalAlias}}}'\] = {{else}}{{#if objectToExport}}\\(.*\\){{/if}}{{/if}}!\\1!g" \
+    -e 's!{{{cjsDependencies.wrapped}}}!require("jquery")!g' \
+    -e 's!{{{globalDependencies.normal}}}!jQuery!g' \
+    -e 's!{{dependencies}}!$!g' \
+    -e 's!{{{objectToExport}}}!SimpleModule!g' \
+    -e '/{{{code}}}/r /dev/stdin' \
+    -e '/{{/d' \
+    build/templates/umd.hbs <src/simple-module.js >dist/simple-module.js
