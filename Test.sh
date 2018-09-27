75b93a52b446b092fd7ac85189f4771edb3536f2
mirabilos <t.glaser@tarent.de>
2018-09-27 17:02:32 +0200

    further…

--- Test.sh
+++ Test.sh
@@ -4,5 +4,11 @@ set -ex
 
 cd "$(dirname "$0")"
 coffee -c spec/simple-module-spec.coffee
+(
+	export NODE_PATH=$PWD/external
+	cd external/xml-name-validator
+	nodejs scripts/generate-grammar.js \
+	    <lib/grammar.pegjs >lib/generated-parser.js
+)
 mocha --require spec/helpers.js spec/simple-module-spec.js
 rm spec/simple-module-spec.js
