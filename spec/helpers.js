4035d7da18077b9a5753b113b6cb34fa3532686c
mirabilos <t.glaser@tarent.de>
2018-09-27 16:29:50 +0200

    successfully inject a required module not available in Debian

--- spec/helpers.js
+++ spec/helpers.js
@@ -1,8 +1,24 @@
+/* jQuery required for tests, which requires a full DOM */
 var jquery = require('jquery');
+/* XHTML/1.1-compatible, minimal, HTML document */
 var minHTML = '<html><head><title>testsuite</title></head><body><p>Hello, World!</p></body></html>';
 
+/* inject parse5 into the global Node.JS module loader */
+var Module = require('module').Module;
+var oldResolveFilename = Module._resolveFilename;
+var cwd = process.cwd();
+Module._resolveFilename = function _resolveFilename_patched(request, parent, isMain) {
+	if (request == 'parse5')
+		request = cwd + '/external/parse5/index.js';
+	return (oldResolveFilename(request, parent, isMain));
+};
+
+/* load jsdom and instantiate a DOM */
 var jsdom = require('../external/jsdom-no-contextify/lib/jsdom.js');
 var doc = jsdom.jsdom(minHTML);
 
+/* instantiate jQuery with the (virtual) DOM */
 global.$ = jquery(doc.parentWindow);
+
+/* load test suite stuff */
 global.expect = require("chai").expect
